2a2322fe9f89e6f1ec3ae5b6547cb5c727e45362
Aybars AKGÜN <aybars.akgun77@gmail.com>
2020-11-20 22:57:53 +0300

    App configs added

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -2,5 +2,5 @@ export const environment = {
   production: true
 };
 
-export const BASE_URL = 'http://student-permits.herokuapp.com';
+export const BASE_URL = 'https://student-permits.herokuapp.com';
 export const WS_BASE_URL = 'ws://student-permits.herokuapp.com';
